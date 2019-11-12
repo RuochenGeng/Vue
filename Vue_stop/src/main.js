de9c66df051736b85ec8a5fee1ada6ee6da42c5b
ruochen <gzy19960601@qq.com>
2019-11-12 14:14:43 +0800

    商品页面完成

--- Vue_stop/src/main.js
+++ Vue_stop/src/main.js
@@ -10,6 +10,12 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 导入TreeTable
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 // 配置请求后的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -21,6 +27,20 @@ Vue.prototype.$http = axios
 Vue.component('tree-table', TreeTable)
 
 Vue.config.productionTip = false
+
+// 格式化时间
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
